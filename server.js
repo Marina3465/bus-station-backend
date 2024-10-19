5e19721347334ea9adb055e7b3bf212a20e0479e
Marina3465 <kozhyxarm12@gmail.com>
2024-10-19 19:25:54 +0300

    add query get routes with parametrs

--- server.js
+++ server.js
@@ -27,14 +27,61 @@ db.connect((err) => {
 });
 
 // Пример маршрута для получения данных
-app.get('/api/data', (req, res) => {
-  db.query('SELECT * FROM `Drivers`', (err, results) => {
+app.get('/routes', (req, res) => {
+  const { start_stop_id, end_stop_id, date } = req.query;
+
+  if (!start_stop_id || !end_stop_id || !date) {
+    return res.status(400).json({ error: 'Both start_stop_id and end_stop_id are required.' });
+  }
+
+  const query = `
+   SELECT 
+    sr1.route_id,
+    r.name AS route_name,
+    s1.name AS start_stop_name,
+    s2.name AS end_stop_name,
+    sr1.departure, 
+    sr2.arrival, 
+    r.standard_price AS base_price,
+    r.standard_price + IFNULL(SUM(sr_add.additional_price), 0) AS total_price 
+FROM 
+    Stops_Routes sr1
+JOIN 
+    Stops_Routes sr2 ON sr1.route_id = sr2.route_id 
+JOIN 
+    Routes r ON sr1.route_id = r.id_route 
+JOIN 
+    Stops s1 ON sr1.stop_id = s1.id_stop 
+JOIN 
+    Stops s2 ON sr2.stop_id = s2.id_stop 
+LEFT JOIN 
+    Stops_Routes sr_add ON sr1.route_id = sr_add.route_id 
+    AND sr_add.stop_order >= sr1.stop_order 
+    AND sr_add.stop_order <= sr2.stop_order 
+WHERE 
+    sr1.stop_id = ${start_stop_id} 
+    AND sr2.stop_id = ${end_stop_id} 
+    AND sr1.stop_order < sr2.stop_order 
+    AND DATE(sr1.departure) = ${date} 
+GROUP BY 
+    sr1.route_id, sr1.departure, sr2.arrival
+ORDER BY 
+    sr1.departure ASC
+
+  `;
+
+  db.query(query, [start_stop_id, end_stop_id, date], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
+
     res.json(results);
   });
 });
+
+
+
+
 
 // Запуск сервера
 app.listen(port, () => {
