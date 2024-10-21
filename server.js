730ffaeb73fe47de41682389055bc364498b3864
Marina3465 <kozhyxarm12@gmail.com>
2024-10-21 15:06:40 +0300

    add input text bus stop

--- server.js
+++ server.js
@@ -28,14 +28,49 @@ db.connect((err) => {
 
 // Пример маршрута для получения данных
 app.get('/routes', (req, res) => {
-  const { start_stop_id, end_stop_id, date } = req.query;
+  const { start_stop_name, end_stop_name, date } = req.query;
+  
 
-  if (!start_stop_id || !end_stop_id || !date) {
+  if (!end_stop_name || !end_stop_name || !date) {
     return res.status(400).json({ error: 'Both start_stop_id and end_stop_id are required.' });
   }
 
-  const query = `
-   SELECT 
+  // const query = `
+  //     SELECT 
+  //       sr1.route_id,
+  //       r.name AS route_name,
+  //       s1.name AS start_stop_name,
+  //       s2.name AS end_stop_name,
+  //       sr1.departure, 
+  //       sr2.arrival, 
+  //       r.standard_price AS base_price,
+  //       r.standard_price + IFNULL(SUM(sr_add.additional_price), 0) AS total_price 
+  //   FROM 
+  //       Stops_Routes sr1
+  //   JOIN 
+  //       Stops_Routes sr2 ON sr1.route_id = sr2.route_id 
+  //   JOIN 
+  //       Routes r ON sr1.route_id = r.id_route 
+  //   JOIN 
+  //       Stops s1 ON sr1.stop_id = s1.id_stop 
+  //   JOIN 
+  //       Stops s2 ON sr2.stop_id = s2.id_stop 
+  //   LEFT JOIN 
+  //       Stops_Routes sr_add ON sr1.route_id = sr_add.route_id 
+  //       AND sr_add.stop_order >= sr1.stop_order 
+  //       AND sr_add.stop_order <= sr2.stop_order 
+  //   WHERE 
+  //       sr1.stop_id = ${start_stop_id} 
+  //       AND sr2.stop_id = ${end_stop_id} 
+  //       AND sr1.stop_order < sr2.stop_order 
+  //       AND DATE(sr1.departure) = '${date}' 
+  //   GROUP BY 
+  //       sr1.route_id, sr1.departure, sr2.arrival
+  //   ORDER BY 
+  //       sr1.departure ASC
+
+  //     `;
+  const query = `SELECT 
     sr1.route_id,
     r.name AS route_name,
     s1.name AS start_stop_name,
@@ -59,18 +94,17 @@ LEFT JOIN
     AND sr_add.stop_order >= sr1.stop_order 
     AND sr_add.stop_order <= sr2.stop_order 
 WHERE 
-    sr1.stop_id = ${start_stop_id} 
-    AND sr2.stop_id = ${end_stop_id} 
+    s1.name = '${start_stop_name}' -- фильтрация по названию начальной остановки
+    AND s2.name = '${end_stop_name}' -- фильтрация по названию конечной остановки
     AND sr1.stop_order < sr2.stop_order 
-    AND DATE(sr1.departure) = ${date} 
+    AND DATE(sr1.departure) = '${date}' 
 GROUP BY 
     sr1.route_id, sr1.departure, sr2.arrival
 ORDER BY 
-    sr1.departure ASC
+    sr1.departure ASC;
+`
 
-  `;
-
-  db.query(query, [start_stop_id, end_stop_id, date], (err, results) => {
+  db.query(query, [start_stop_name, end_stop_name, date], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
@@ -79,7 +113,25 @@ ORDER BY
   });
 });
 
+// Пример маршрута для получения данных
+app.get('/stop', (req, res) => {
+  const { stop_name } = req.query;
 
+  if (!stop_name) {
+    return res.status(400).json({ error: 'Both start_stop_id and end_stop_id are required.' });
+  }
+
+  const query = `
+      SELECT id_stop FROM Stops WHERE name='${stop_name}'
+`
+  db.query(query, [stop_name], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.json(results);
+  });
+});
 
 
 
